bffe54199ec07dacdea55a52c48676dc22016404
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2024-05-16 08:27:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CS 12 23.2 Lab 10
+
+## Prerequisites
+
+Navigate to your local repo root, then run the following _(replace `pip` with `pip3` or `python -m pip` if needed)_:
+```bash
+pip install -r requirements.txt
+```
---
+++ pipe.py
@@ -0,0 +1,78 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from random import Random, getrandbits
+from typing import Final
+
+import pyxel
+
+import pyxelgrid as pg
+import pipelib as pl
+
+
+TITLE: Final[str] = "Pipe Dream"
+FPS: Final[int] = 25
+
+
+@dataclass
+class State:
+    ... # TODO
+
+
+class PipeGame(pg.PyxelGrid[State]):
+    def __init__(self, settings: pl.DifficultySettings, seed: int) -> None:
+        self.settings = settings
+        self.seed = seed
+        self.rand = Random(seed)
+
+        ... # TODO
+
+        super().__init__(settings.r, settings.c, dim=pl.DIM)
+
+    def init(self) -> None:
+        pyxel.mouse(True)
+        pyxel.load(pl.PIPE_RESOURCE_PATH)
+        pyxel.rseed(self.seed)
+
+        ... # TODO
+
+    def update(self) -> None:
+
+        # TODO remove this debugging message
+        print("The mouse is at cell", self.mouse_cell())
+
+        ... # TODO
+
+    def draw_cell(self, i: int, j: int, x: int, y: int) -> None:
+
+        ... # TODO
+
+    def pre_draw_grid(self) -> None:
+        
+        ... # TODO
+
+    def post_draw_grid(self) -> None:
+        
+        ... # TODO
+
+
+def main():
+    parser = ArgumentParser(description=f"Run the '{TITLE}' game.")
+
+    parser.add_argument('-d', '--difficulty', default='easy',
+        help=f"the game difficulty. must be one of: {', '.join(pl.DIFFICULTY_SETTINGS.keys())} (default: %(default)s)")
+    parser.add_argument('-s', '--seed', type=int, default=None,
+        help="the seed for the random number generator. (default: based on system time)")
+
+    args = parser.parse_args()
+
+    if (seed := args.seed) is None:
+        seed = getrandbits(31)
+
+    if args.difficulty not in pl.DIFFICULTY_SETTINGS:
+        raise ValueError(f"Unknown difficulty: {args.difficulty}")
+
+    PipeGame(settings=pl.DIFFICULTY_SETTINGS[args.difficulty], seed=seed).run(title=TITLE, fps=FPS)
+
+
+if __name__ == '__main__':
+    main()
